6fd16b1190cb021c1ca518fa74694626d60ad73f
Ari Abril <81791043+AriAbril@users.noreply.github.com>
2022-02-15 17:51:55 -0600

    Add files via upload

---
+++ js/ejercicio04.js
@@ -0,0 +1,13 @@
+/**
+ * Del siguiente arreglo de número obtener lo siguiente: media, mediana y moda
+ * El formato de la respuesta es:
+ * {
+ * media: 0,
+ * mediana: 0,
+ * moda: 0,
+ * }
+ *
+ * NOTA: solo se puede utilizar: sort, filter, pop y reduce, para interactuar con el arreglo
+ */
+ 
+const datos = [12, 45, 17, 7, 22, 71, 50, 37, 10, 48, 17, 51, 17, 30, 12, 81];
