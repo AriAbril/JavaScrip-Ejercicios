fdebab0c48c22b615a94a438ce18781a61f92053
Abril Ocampo <alos@gft.com>
2022-02-16 12:17:25 -0600

    odio aquí

--- js/ejercicio04.js
+++ js/ejercicio04.js
@@ -21,31 +21,31 @@ console.log(`La media es: ${media}`);
 const numOrdenados = datos.sort((num1, num2) => {
     return num1 - num2;
 });
-console.log(numOrdenados);
+// console.log(numOrdenados);  // Imprime el arreglo con los números ya ordenados de menor a mayor
 
 let mediana = 0;
 if (numOrdenados.length%2==0){
-    console.log('par');
+    // console.log('par');  // Imprime si el número es par o impar
     const ind1 = numOrdenados.length / 2 -1;
     const ind2 = ind1+1;
-    console.log(ind1, ind2);
+   // console.log(ind1, ind2); // Imprime las posiciones de los números para tomar la mediana
     const medianas = [numOrdenados[ind1],numOrdenados[ind2]];
     const mediaMediana = medianas.reduce((previous, current) => current+= previous);
     mediana = mediaMediana/medianas.length;
-    console.log(mediana);
+    // console.log(mediana); // imprime la mediana
 } else {
     console.log('impar');
     const ind = numOrdenados.length / 2;
     mediana = numOrdenados[Math.trunc(ind)];// el math.trunc elimina los decimales
 };
-    console.log(mediana); 
+console.log(`La mediana es: ${mediana}`); 
 
 //moda
     const moda = datos.sort((a,b) => // orden de elementos de acuerdo a su frecuencia en orden ascendente para que el que aparezca más veces esté al final
             datos.filter(v => v===a).length // conteo de frecuencia del elemento a 
             - datos.filter(v => v===b).length // conteo de frecuencia del elemento b
         ).pop(); // Pone el elemento que más se repite al final del arreglo 
-    console.log(moda); 
+        console.log(`La moda es: ${moda}`);
 
 // formato 
 const formato = {
