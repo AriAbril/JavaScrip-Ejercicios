24a4e571b245d84e7a00d88f378285d71479e9a0
Abril Ocampo <alos@gft.com>
2022-02-16 16:26:23 -0600

    ejercicio 5

---
+++ js/ejercicio05.js
@@ -0,0 +1,45 @@
+/**
+ * Del siguiente arreglo obtener el número de veces que cada sabor de helado
+ * aparece.
+ * {
+ * "Strawberry": 3,
+ * "Vanilla": 4,
+ * "Chocolate": 5,
+ * "Cookies & Cream": 2,
+ * "Mint Chocolate Chip": 3,
+ * "Rocky Road": 1,
+ * "Pistachio": 1,
+ * "Banana": 1,
+ * "French Vanilla": 1,
+ * "Vanilla Bean": 1
+ * }
+ *
+ * NOTA: el arreglo solo con reduce, dentro sí se pueden usar otros statements
+ */
+ 
+ const data = [{
+    name: 'Tyler',
+    favoriteIceCreams: ['Strawberry', 'Vanilla', 'Chocolate', 'Cookies & Cream']
+   },
+   {
+    name: 'Richard',
+    favoriteIceCreams: ['Cookies & Cream', 'Mint Chocolate Chip', 'Chocolate', 'Vanilla']
+   },
+   {
+    name: 'Amanda',
+    favoriteIceCreams: ['Chocolate', 'Rocky Road', 'Pistachio', 'Banana']
+   },
+   {
+    name: 'Andrew',
+    favoriteIceCreams: ['Vanilla', 'Chocolate', 'Mint Chocolate Chip']
+   },
+   {
+    name: 'David',
+    favoriteIceCreams: ['Vanilla', 'French Vanilla', 'Vanilla Bean', 'Strawberry']
+   },
+   {
+    name: 'Karl',
+    favoriteIceCreams: ['Strawberry', 'Chocolate', 'Mint Chocolate Chip']
+   }
+   ];
+
