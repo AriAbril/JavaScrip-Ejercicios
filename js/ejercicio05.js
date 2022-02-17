7b6253066a7a98665917626183a4cd8b7017bb66
Ari Abril <occamppo.3025@gmail.com>
2022-02-16 19:54:23 -0600

    ejercicio 5

--- js/ejercicio05.js
+++ js/ejercicio05.js
@@ -16,30 +16,53 @@
  *
  * NOTA: el arreglo solo con reduce, dentro sí se pueden usar otros statements
  */
- 
- const data = [{
-    name: 'Tyler',
-    favoriteIceCreams: ['Strawberry', 'Vanilla', 'Chocolate', 'Cookies & Cream']
+
+   const data = [{
+      name: 'Tyler',
+      favoriteIceCreams: ['Strawberry', 'Vanilla', 'Chocolate', 'Cookies & Cream']
    },
    {
-    name: 'Richard',
-    favoriteIceCreams: ['Cookies & Cream', 'Mint Chocolate Chip', 'Chocolate', 'Vanilla']
+      name: 'Richard',
+      favoriteIceCreams: ['Cookies & Cream', 'Mint Chocolate Chip', 'Chocolate', 'Vanilla']
    },
    {
-    name: 'Amanda',
-    favoriteIceCreams: ['Chocolate', 'Rocky Road', 'Pistachio', 'Banana']
+      name: 'Amanda',
+      favoriteIceCreams: ['Chocolate', 'Rocky Road', 'Pistachio', 'Banana']
    },
    {
-    name: 'Andrew',
-    favoriteIceCreams: ['Vanilla', 'Chocolate', 'Mint Chocolate Chip']
+      name: 'Andrew',
+      favoriteIceCreams: ['Vanilla', 'Chocolate', 'Mint Chocolate Chip']
    },
    {
-    name: 'David',
-    favoriteIceCreams: ['Vanilla', 'French Vanilla', 'Vanilla Bean', 'Strawberry']
-   },
+      name: 'David',
+      favoriteIceCreams: ['Vanilla', 'French Vanilla', 'Vanilla Bean', 'Strawberry']
+   } ,
    {
-    name: 'Karl',
-    favoriteIceCreams: ['Strawberry', 'Chocolate', 'Mint Chocolate Chip']
+      name: 'Karl',
+      favoriteIceCreams: ['Strawberry', 'Chocolate', 'Mint Chocolate Chip']
    }
    ];
 
+let initial = 
+   {
+      "Strawberry": 0,
+      "Vanilla": 0,
+      "Chocolate": 0,
+      "Cookies & Cream": 0,
+      "Mint Chocolate Chip": 0,
+      "Rocky Road": 0,
+      "Pistachio": 0,
+      "Banana": 0,
+      "French Vanilla": 0,
+      "Vanilla Bean": 0
+   }   
+
+const flavorFreq = data.reduce((previus, current) =>{
+
+   current.favoriteIceCreams.map((favoriteIceCream) =>{ previus[favoriteIceCream]++;}); // lee el arreglo y realiza un incremento cuando encuentra los sabores.
+
+   return previus; // retorna el sabor previo
+
+}, initial ); // concatena el formato
+
+console.log(flavorFreq); 
